6bdd3286a2c7b85885ab9effc54258d08bb7e3bb
Dalaias <dalaiaas1327@gmail.com>
2021-08-03 19:38:36 -0600

    subiendo TAREA1 C+

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    double n1, n2;
+    
+    
+    cout<<"Ingrese primer numero: ";
+    cin>>n1;
+    cout<<"ingrese segundo numero: ";
+    cin>>n2;
+    
+    double suma = n1 + n2;
+    cout<<"la suma es: "<<suma<<endl;
+    double resta = n1 - n2;
+    cout<<"la resta es: "<<resta<<endl;
+    double multiplicacion = n1 * n2;
+    cout<<"la multiplicacion es: "<<multiplicacion<<endl;
+    double division = n1 / n2;
+    cout<<"la division es: "<<division<<endl;
+    
+        
+	return 0;
+}
